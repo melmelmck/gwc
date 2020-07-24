b8c82767bdd0c03002e04ac57ad1038ea2c2e95e
madelinemckune <madelinemckune@mymail.mines.edu>
2020-07-23 18:49:09 -0700

    added files

---
+++ public/index.js
@@ -0,0 +1,20 @@
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
+const docRef = db.collection('users').doc('alovelace');
+
+await docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
+const aTuringRef = db.collection('users').doc('aturing');
+
+await aTuringRef.set({
+  'first': 'Alan',
+  'middle': 'Mathison',
+  'last': 'Turing',
+  'born': 1912
+});
+  
